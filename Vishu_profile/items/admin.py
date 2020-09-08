562fe84235b02ee0d5dc2a1219b5e41ef98c19af
Raja <rajavishu25@outlook.com>
2020-09-08 22:47:18 +0530

    admin.py filled with classes which take models

--- Vishu_profile/items/admin.py
+++ Vishu_profile/items/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Education
+from .models import Experience
+from .models import Language
+from .models import Skill
+from .models import Extracurricular_and_hobby
+
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Extracurricular_and_hobby)
+class ExtracurricularAdmin(admin.ModelAdmin):
+    pass
