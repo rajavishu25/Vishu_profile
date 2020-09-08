24c0686fbd0e7226b8b0d08d9e029af26b7c93e5
Raja <rajavishu25@outlook.com>
2020-09-08 21:48:43 +0530

    added all the reqruired models

--- Vishu_profile/items/models.py
+++ Vishu_profile/items/models.py
@@ -10,4 +10,26 @@ class Education(models.Model):
     image = models.ImageField(upload_to = 'images/')
     thesis = models.CharField(max_length= 200,blank = True)
     thesis_description = models.TextField(blank = True)
+
+class Experience(models.Model):
+    name = models.CharField(max_length = 200)
+    company = models.CharField(max_length = 200)
+    starttime = models.DateField()
+    endtime = models.DateField()
+    description = models.TextField()
+    website = models.URLField(blank = True)
+
+class Skill(models.Model):
+    skill_name = models.CharField(max_length = 100)
+    items= models.TextField()
+            
+class Language(models.Model):
+    name = models.CharField(max_length = 50)
+    proficiency_choices = ['beginner','intermediate','proficient','native']
+    proficiency = models.CharField(max_length = 20, choices = proficiency_choices)
+
+class Extracurricular_and_hobby(models.Model):
+    items = models.TextField()
+
+
     
