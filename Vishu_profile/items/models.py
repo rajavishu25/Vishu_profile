e553a209474743f3274d9a8588399cab4a288127
Raja <rajavishu25@outlook.com>
2020-09-08 17:02:40 +0530

    added Education class in models

--- Vishu_profile/items/models.py
+++ Vishu_profile/items/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Education(models.Model):
+    name = models.CharField(max_length= 200)
+    university = models.CharField(max_length = 200)
+    university_description = models.TextField(blank = True)
+    specialization = models.CharField(max_length = 200)
+    graduated_year = models.IntegerField()
+    competencies = models.TextField()
+    image = models.ImageField(upload_to = 'images/')
+    thesis = models.CharField(max_length= 200,blank = True)
+    thesis_description = models.TextField(blank = True)
+    
