1725b8c5b11dde71832905be2c293e7ef720e760
Raja <rajavishu25@outlook.com>
2020-09-08 23:19:25 +0530

    Added string representations for model classes so that we know what they are in admin

--- Vishu_profile/items/models.py
+++ Vishu_profile/items/models.py
@@ -11,6 +11,9 @@ class Education(models.Model):
     thesis = models.CharField(max_length= 200,blank = True)
     thesis_description = models.TextField(blank = True)
 
+    def __str__(self):
+        return self.name
+
 class Experience(models.Model):
     name = models.CharField(max_length = 200)
     company = models.CharField(max_length = 200)
@@ -19,17 +22,29 @@ class Experience(models.Model):
     description = models.TextField()
     website = models.URLField(blank = True)
 
+    def __str__(self):
+        return self.name
+
 class Skill(models.Model):
     skill_name = models.CharField(max_length = 100)
     items= models.TextField()
+
+    def __str__(self):
+        return self.skill_name
             
 class Language(models.Model):
     name = models.CharField(max_length = 50)
     proficiency_choices = [('1','beginner'),('2','intermediate'),('3', 'proficient'),('4','native')]
     proficiency = models.CharField(max_length = 20, choices = proficiency_choices)
 
+    def __str__(self):
+        return self.name
+
 class Extracurricular_and_hobby(models.Model):
     items = models.TextField()
+
+    def __str__(self):
+        return "Extra curricular activities and Hobbies"
 
 
     
