8e05a8be5779599be281f36ab15572e6ac040856
Raja <rajavishu25@outlook.com>
2020-09-09 22:54:44 +0530

    views updated to send info to templates

--- Vishu_profile/items/views.py
+++ Vishu_profile/items/views.py
@@ -4,3 +4,21 @@ from .models import Education,Experience, Skill, Extracurricular_and_hobby, Lang
 def home(request):
     return render(request,'home.html')
 
+def education(request):
+    education = Education.objects.all
+    return render(request,'education.html',{'education': education })
+
+def experience(request):
+    experience = Experience.objects.all
+    return render(request, 'experience.html',{'experience' : experience})
+
+def skills(request):
+    skills = Skill.objects.all
+    languages = Language.objects.all
+    return render(request, 'skills.html', {'skills' : skills, 'languages': languages})
+
+def hobbies(request):
+    hobbies = Extracurricular_and_hobby.objects.all
+    return render(request, 'hobbies.html', {'hobbies' : hobbies})
+
+
