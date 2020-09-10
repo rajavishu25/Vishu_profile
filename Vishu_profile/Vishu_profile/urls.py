605bf2d0fc43df8c19fcffee8caad0673f3f13cc
Raja <rajavishu25@outlook.com>
2020-09-10 11:50:15 +0530

    changes done to url and settings so that media from the db is displayed on the website

--- Vishu_profile/Vishu_profile/urls.py
+++ Vishu_profile/Vishu_profile/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from items import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,7 @@ urlpatterns = [
     path('skills', views.skills, name = 'skill'),
     path('hobbies', views.hobbies, name = 'hobbies'),
     
-]
+]  
+
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
